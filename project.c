7c28ff7d608cbab99b57fa861e9166751bbb7920
SahilR123 <49682622+SahilR123@users.noreply.github.com>
2019-04-23 08:10:07 +0530

    Update project.c

--- project.c
+++ project.c
@@ -986,3 +986,17 @@ another=getchar();
 }
 
 }
+/*
+void setcolor(int ForgC)
+{
+     WORD wColor;
+     HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+     CONSOLE_SCREEN_BUFFER_INFO csbi;
+     if(GetConsoleScreenBufferInfo(hStdOut, &csbi))
+     {
+          wColor = (csbi.wAttributes & 0xF0) + (ForgC & 0x0F);
+          SetConsoleTextAttribute(hStdOut, wColor);
+     }
+}
+setcolor(10);//this goes in main
+*/
